136e6f68858eecd03c474fbf0e0a4dbebd4f86d4
ByMartrixx <bymartrixx@gmail.com>
2020-12-17 23:14:21 -0300

    Update tabs

--- src/main/java/io/github/bymartrixx/vtd/gui/VTDScreen.java
+++ src/main/java/io/github/bymartrixx/vtd/gui/VTDScreen.java
@@ -8,7 +8,13 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 
 public class VTDScreen extends Screen {
+    private static int getMaxTabNum(int width) {
+        // The 120 is because of the two 40 px wide buttons and the spacing between
+        return (width - 120) / 120;
+    }
+
     private final Screen previousScreen;
+    private int tabIndex = 0;
 
     public VTDScreen(Screen previousScreen) {
         super(new LiteralText("VTDownloader"));
@@ -22,11 +28,19 @@ public class VTDScreen extends Screen {
     protected void init() {
         this.addButton(new ButtonWidget(this.width - 130, this.height - 30, 120, 20, new LiteralText("Done"), button -> this.onClose()));
 
-        for (int i = 0; i < VTDMod.categories.size(); ++i) {
-            JsonObject category = VTDMod.categories.get(i).getAsJsonObject();
+        this.addButton(new ButtonWidget(10, 10, 40, 20, new LiteralText("<-"), button -> {
+            // TODO
+        }));
+        this.addButton(new ButtonWidget(60, 10, 40, 20, new LiteralText("->"), button -> {
+            // TODO
+        }));
+
+        for (int i = 0; i < getMaxTabNum(this.width); ++i) {
+            int index = i + tabIndex;
+            JsonObject category = VTDMod.categories.get(index).getAsJsonObject();
             String categoryName = category.get("category").getAsString();
 
-            this.addButton(new ButtonWidget(i * 130 + 10, 10, 120, 20, new LiteralText(categoryName), button -> {
+            this.addButton(new ButtonWidget(index * 130 + 110, 10, 120, 20, new LiteralText(categoryName), button -> {
                 // TODO
             }));
         }
