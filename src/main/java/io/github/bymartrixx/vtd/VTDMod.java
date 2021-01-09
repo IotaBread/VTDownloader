c51b030ec716832f2bc86276db8e434b5270e302
ByMartrixx <bymartrixx@gmail.com>
2021-01-09 20:03:20 -0300

    Reformat code and add request messages

--- src/main/java/io/github/bymartrixx/vtd/VTDMod.java
+++ src/main/java/io/github/bymartrixx/vtd/VTDMod.java
@@ -21,12 +21,12 @@ public class VTDMod implements ClientModInitializer {
     public static final String MOD_NAME = "VTDownloader";
     public static final Logger LOGGER = LogManager.getLogger();
     public static final Gson GSON = new Gson();
-    public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).isPresent() ? FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().toString() : "1.0.0";
+    public static final String VERSION = FabricLoader.getInstance().getModContainer(VTDMod.MOD_ID).isPresent() ? FabricLoader.getInstance().getModContainer(VTDMod.MOD_ID).get().getMetadata().getVersion().toString() : "1.0.0";
     public static final String BASE_URL = "https://vanillatweaks.net";
     public static JsonArray rpCategories;
 
     public static void log(Level level, String message, Object... fields) {
-        LOGGER.log(level, "[" + MOD_NAME + "] " + message, fields);
+        VTDMod.LOGGER.log(level, "[" + VTDMod.MOD_NAME + "] " + message, fields);
     }
 
     public static void log(Level level, String message) {
@@ -34,19 +34,19 @@ public class VTDMod implements ClientModInitializer {
     }
 
     public static void logError(String message, Throwable t) {
-        LOGGER.log(Level.ERROR, "[" + MOD_NAME + "] " + message, t);
+        VTDMod.LOGGER.log(Level.ERROR, "[" + VTDMod.MOD_NAME + "] " + message, t);
     }
 
     public static JsonArray getCategories(String resourceUrl) throws IOException {
         CloseableHttpClient client = HttpClients.createDefault();
 
-        HttpGet request = new HttpGet(BASE_URL + resourceUrl);
+        HttpGet request = new HttpGet(VTDMod.BASE_URL + resourceUrl);
         HttpResponse response = client.execute(request);
 
         int responseStatusCode = response.getStatusLine().getStatusCode();
 
         if (responseStatusCode / 100 != 2) { // Check if responseStatusCode is 2xx/Success
-            VTDMod.log(Level.WARN, "The request to the URL {} responded with an unexpected status code: {}. The request processing has been canceled.", BASE_URL + resourceUrl, responseStatusCode);
+            VTDMod.log(Level.WARN, "The request to the URL {} responded with an unexpected status code: {}. The request processing has been canceled.", VTDMod.BASE_URL + resourceUrl, responseStatusCode);
             return new JsonArray(); // Prevent NPE
         }
 
@@ -57,28 +57,30 @@ public class VTDMod implements ClientModInitializer {
             responseContent.append(responseScanner.nextLine());
         }
 
-        return GSON.fromJson(responseContent.toString(), JsonObject.class).get("categories").getAsJsonArray();
+        return VTDMod.GSON.fromJson(responseContent.toString(), JsonObject.class).get("categories").getAsJsonArray();
     }
 
     public static void getRPCategories() throws IOException {
-        rpCategories = VTDMod.getCategories("/assets/resources/json/1.16/rpcategories.json");
+        VTDMod.rpCategories = VTDMod.getCategories("/assets/resources/json/1.16/rpcategories.json");
     }
 
     public static void reloadRPCategories() {
         try {
-            getRPCategories();
+            VTDMod.log(Level.INFO, "Requesting Resource pack categories and packs.");
+            VTDMod.getRPCategories();
+            VTDMod.log(Level.INFO, "Resource pack categories and packs loaded. There are {} Resource pack categories.", VTDMod.rpCategories.size());
         } catch (IOException e) {
-            logError("Encountered an exception while getting the resource pack categories.", e);
-            rpCategories = new JsonArray(); // Prevent NPE
+            VTDMod.logError("Encountered an exception while getting the Resource pack categories.", e);
+            VTDMod.rpCategories = new JsonArray(); // Prevent NPE
         }
     }
 
     @Override
     public void onInitializeClient() {
-        log(Level.INFO, "Initializing {} version {}...", MOD_NAME, VERSION);
+        VTDMod.log(Level.INFO, "Initializing {} version {}...", VTDMod.MOD_NAME, VTDMod.VERSION);
 
-        reloadRPCategories();
+        VTDMod.reloadRPCategories();
 
-        log(Level.INFO, "Initialized {} version {}", MOD_NAME, VERSION);
+        VTDMod.log(Level.INFO, "Initialized {} version {}", VTDMod.MOD_NAME, VTDMod.VERSION);
     }
 }
